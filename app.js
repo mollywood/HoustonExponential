a6d067c957e6e75a406c71f3a723779ac590c4df
jet <jetsetta@Jets-MacBook-Air.local>
2018-08-13 14:59:35 -0500

    added dependencies

--- app.js
+++ app.js
@@ -1,2 +1,28 @@
-const express = require("express");
-const routes = require("./routes");
+const express = require('express')
+const bodyParser = require('body-parser')
+const pg = require('pg')
+const app = express()
+let db = require('./models/index.js')
+
+db.User.findOrCreate({ where:
+  {firstName: 'Test'},
+  defaults:
+  {lastName: 'Testy',
+  email: 'test@gmail.com',
+  password: 'qwerty1',
+  password2: 'qwerty1'}
+}).spread((user, created) => {
+    console.log(user.get({
+      plain: true
+    }))
+})
+
+
+
+
+// User
+//   .findOrCreate({where: {username: 'sdepold'}, defaults: {job: 'Technical Lead JavaScript'}})
+//   .spread((user, created) => {
+//     console.log(user.get({
+//       plain: true
+//     }))
