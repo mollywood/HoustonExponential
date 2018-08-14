596ac793fd892d31de9c70fea4451ea536ab62ca
Shawn Benny <shawn@Shawns-MacBook-Pro.local>
2018-08-14 14:38:01 -0500

    LinkedIn

--- app.js
+++ app.js
@@ -4,11 +4,44 @@ const body_parser = require("body-parser");
 const exphbs = require("express-handlebars");
 const db = require("./models/index.js");
 const app = express();
+const passport = require("passport");
+var LinkedInStrategy = require("passport-linkedin-oauth2").Strategy;
 
-// Investor is the model name defined in the models/investor.js
-db.Investor.create({ name: "Katy" }).then(result => {
-  console.log(result);
+app.use(express.static("public"));
+// app.use(express.cookieParser());
+// app.use(express.bodyParser());
+// app.use(express.session({ secret: "keyboard cat" }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
 });
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.use(
+  new LinkedInStrategy(
+    {
+      clientID: "78umadn462runs",
+      clientSecret: "JzPt9cNwIpvkSQZ1",
+      callbackURL: "http://127.0.0.1:8000/auth/linkedin/callback",
+      scope: ["r_emailaddress", "r_basicprofile"]
+    },
+    function(accessToken, refreshToken, profile, done) {
+      // asynchronous verification, for effect...
+      process.nextTick(function() {
+        // To keep the example simple, the user's LinkedIn profile is returned to
+        // represent the logged-in user. In a typical application, you would want
+        // to associate the LinkedIn account with a user record in your database,
+        // and return that user instead.
+        return done(null, profile);
+      });
+    }
+  )
+);
 
 //Middleware
 app.use(express.static("public"));
@@ -29,6 +62,7 @@ app.set("view engine", ".hbs");
 app.get("/api/data", (req, res) => {});
 
 app.get("/", (req, res) => {
+  console.log(req.user);
   res.render("home", {
     title: "Welcome",
     message: "Hello world",
@@ -43,6 +77,27 @@ app.get("/register", (req, res) => {
     subheading: "It's nice to meet you"
   });
 });
+
+app.get(
+  "/auth/linkedin",
+  passport.authenticate("linkedin", { state: "SOME STATE" }),
+  function(req, res) {
+    // The request will be redirected to LinkedIn for authentication, so this
+    // function will not be called.
+  }
+);
+
+app.get(
+  "/auth/linkedin/callback",
+  passport.authenticate("linkedin", {
+    successRedirect: "/",
+    failureRedirect: "/register"
+  })
+);
+
+// app.get("auth/linkedin/callback/:code", (req, res) => {
+//   console.log(req.params.code);
+// });
 
 app.post("/register", (req, res) => {});
 
