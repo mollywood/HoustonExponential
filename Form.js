a2b0d91dc360e85542154aa7d29882b76cdd8857
Jessica Robinson <jessicapolansky@gmail.com>
2018-08-14 17:26:28 -0500

    saving testing

--- Form.js
+++ Form.js
@@ -1,47 +1,21 @@
-const investor = {
-	Type,
-	Themes,
-	InvestmentRange,
-	ManagedAssets,
-	PortfolioSize,
-	Founded,
-	Location,// Address, Neighborhood, Latitude/Longitude
-	Contact,
-	Description 		
-}
+$(document).ready(function() {
+	let startup = $('#startup').val()
+	let investor = $('#investor').val()
+	let hub = $('#hub').val()
+	if (startup) {
+		$( ".startup" ).show();
+		$( ".investor" ).hide();
+		$( ".hub" ).hide();
+		console.log('startup')
+	} else if (investor) {
+		$( ".investor" ).show();
+		$( ".startup" ).hide();
+		$( ".hub" ).hide();
+		console.log('investor')
 
-const startup = {
-	Name,
-	Logo,
-	Description,
-	Category,
-	Location, //→ Address, Neighborhood, Latitude/Longitude
-	Employees,
-	FundingStage,
-	Founded,
-	BusinessModel, //(B2B2C, B2G)
-	ProductStage,
-	Website,
-	ContactInfo //Phone, Email
-}
-
-const hub = {
-		Type, //(Accelerator, Incubator, Co-working Space)
-		BatchSize,
-		Founded,
-		Location, //→ Address, Neighborhood, Latitude/Longitude
-		Academia, //Supported?
-		Contact,
-		Description 
-};
-const form = (input) => {
-	if (input.id == "startup") {
-		return "startup";
-	} else if (input.id == "investor") {
-		return "investor";
-	} else if (input.id == "hub") {
-		return "hub";
-	} else {
-		throw error;
+	} else if (hub) {
+		$( ".hub" ).show();
+		$( ".startup" ).hide();
+		$( ".investor" ).hide();
 	}
-}
+})
