49baa09f6c3271e54d9bd3cf0499cb5f2254f162
Shawn Benny <shawn@Shawns-MacBook-Pro.local>
2018-08-16 12:30:17 -0500

    adding bio and logo to Invester and Services tables

--- models/company.js
+++ models/company.js
@@ -1,19 +1,24 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  var Company = sequelize.define('Company', {
-    name: DataTypes.STRING,
-    logo: DataTypes.STRING,
-    description: DataTypes.STRING,
-    category: DataTypes.STRING,
-    location: DataTypes.STRING,
-    employees: DataTypes.STRING,
-    fundingstage: DataTypes.STRING,
-    foundedDate: DataTypes.STRING,
-    businessModel: DataTypes.STRING,
-    productStage: DataTypes.STRING,
-    websiteUrl: DataTypes.STRING,
-    contact: DataTypes.STRING
-  }, {});
+  var Company = sequelize.define(
+    "Company",
+    {
+      name: DataTypes.STRING,
+      logo: DataTypes.STRING,
+      description: DataTypes.STRING,
+      category: DataTypes.STRING,
+      location: DataTypes.STRING,
+      employees: DataTypes.STRING,
+      fundingstage: DataTypes.STRING,
+      foundedDate: DataTypes.STRING,
+      businessModel: DataTypes.STRING,
+      productStage: DataTypes.STRING,
+      websiteUrl: DataTypes.STRING,
+      contact: DataTypes.STRING,
+      bio: DataTypes.STRING
+    },
+    {}
+  );
   Company.associate = function(models) {
     // associations can be defined here
   };
