5cbab0bec42a075863e76c40f8873fc3b26c173c
Shawn Benny <shawn@Shawns-MBP.localdomain>
2018-08-19 15:18:39 -0500

    mc

--- models/company.js
+++ models/company.js
@@ -1,9 +1,5 @@
 "use strict";
 module.exports = (sequelize, DataTypes) => {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 059fb4f1759a56d774befe9885e2e817a8655196
   var Company = sequelize.define(
     "Company",
     {
@@ -23,27 +19,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     {}
   );
-  Company.associate = function(models) {
-<<<<<<< HEAD
-=======
-  var Company = sequelize.define('Company', {
-    name: DataTypes.STRING,
-    logo: DataTypes.STRING,
-    description: DataTypes.STRING,
-    category: DataTypes.STRING,
-    location: DataTypes.STRING,
-    employees: DataTypes.STRING,
-    fundingstage: DataTypes.STRING,
-    foundedDate: DataTypes.STRING,
-    businessModel: DataTypes.STRING,
-    productStage: DataTypes.STRING,
-    websiteUrl: DataTypes.STRING,
-    contact: DataTypes.STRING
-  }, {});
-  Company.associate = (models) => {
->>>>>>> master
-=======
->>>>>>> 059fb4f1759a56d774befe9885e2e817a8655196
+  Company.associate = models => {
     // associations can be defined here
   };
   return Company;
