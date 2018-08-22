6cec23e294c81bc3c8bbc8b1f5c40399f6b6bbcf
Shawn Benny <shawn@Shawns-MacBook-Pro.local>
2018-08-22 15:47:55 -0500

    killin em bugs (opacity,footer)

--- routes/registerCompany.js
+++ routes/registerCompany.js
@@ -1,40 +1,40 @@
 const express = require("express");
 const router = express.Router();
 const Sequelize = require("sequelize");
-const db = require('../models/index');
+const db = require("../models/index");
+const validateLogin = require("./routeProtection").validateLogin;
 
-
-router.get('', (req, res) => {
-    res.render("registerCompany", {});
+router.get("", validateLogin, (req, res) => {
+  res.render("registerCompany", {});
 });
 
-router.post("", (req, res) => {
-	db.Company.findOrCreate({
-	  where: {
-		name: req.body.cname
-	  },
-	  defaults: {
-		logo: req.body.logo,
-		description: req.body.cdescription,
-		category: req.body.category,
-		location: req.body.clocation,
-		employees: req.body.employees,
-		fundingstage: req.body.fundingstage,
-		foundedDate: req.body.foundedDate,
-		productStage: req.body.productStage,
-		businessModel: req.body.businessModel,
-		websiteUrl: req.body.websiteUrl,
-		contact: req.body.contact,
-		bio: req.body.bio
-	  }
-	}).spread((company, created) => {
-	  if (created) {
-		res.render("companies", {});
-	  } else {
-		res.render("registerCompany", {
-		  message: "Company already exists"
-		});
-	  }
-	});
+router.post("", validateLogin, (req, res) => {
+  db.Company.findOrCreate({
+    where: {
+      name: req.body.cname
+    },
+    defaults: {
+      logo: req.body.logo,
+      description: req.body.cdescription,
+      category: req.body.category,
+      location: req.body.clocation,
+      employees: req.body.employees,
+      fundingstage: req.body.fundingstage,
+      foundedDate: req.body.foundedDate,
+      productStage: req.body.productStage,
+      businessModel: req.body.businessModel,
+      websiteUrl: req.body.websiteUrl,
+      contact: req.body.contact,
+      bio: req.body.bio
+    }
+  }).spread((company, created) => {
+    if (created) {
+      res.render("companies", {});
+    } else {
+      res.render("registerCompany", {
+        message: "Company already exists"
+      });
+    }
   });
+});
 module.exports = router;
