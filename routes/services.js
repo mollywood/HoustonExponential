5d72f48c2136c93e5b354cb2c30a2fc88f1023fc
Molly Wood <34974982+mollywood@users.noreply.github.com>
2018-08-31 15:56:05 -0500

    final update

--- routes/services.js
+++ routes/services.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 const Sequelize = require("sequelize");
 const db = require("../models/index");
-const validateLogin = require('./routeProtection').validateLogin;
+const validateLogin = require("./routeProtection").validateLogin;
 
 // @route GET routes/services
 // @desc Queries database and displays services on page
 // @access Public
-router.get("", validateLogin, (req, res) => {
+router.get("", (req, res) => {
   db.Service.findAll().then(services => {
     res.render("services", { serviceList: services });
   });
@@ -16,39 +16,40 @@ router.get("", validateLogin, (req, res) => {
 // @route GET routes/services/register
 // @desc Renders registerService.hbs view
 // @access Protected
-router.get("/register", validateLogin, (req, res) => {
-    res.render("registerService", {});
+router.get("/register", (req, res) => {
+  res.render("registerService", {});
 });
 
 // @route POST routes/services/register
 // @desc Posts Service inputs into database
 // @access Protected
 router.post("/register", (req, res) => {
-	db.Service.findOrCreate({
-	  where: {
-		name: req.body.name
-	  },
-	  defaults: {
-		logo: req.body.logo,
-		description: req.body.description,
-		type: req.body.type,
-		location: req.body.location,
-		batchSize: req.body.batchSize,
-		academiaSupported: req.body.academiaSupported,
-		associatedFund: req.body.associatedFund,
-		website: req.body.website,
-		founded: req.body.foundedDate,
-		contact: req.body.contact,
-		bio: req.body.bio,
-	  }
-	}).spread((service, created) => {
-	  if (created) {
-		res.redirect("/services")
-	  } else {
-		  res.render("registerService", {
-		  message: "Service already exists"});
-    };
-	});
+  db.Service.findOrCreate({
+    where: {
+      name: req.body.name
+    },
+    defaults: {
+      logo: req.body.logo,
+      description: req.body.description,
+      type: req.body.type,
+      location: req.body.location,
+      batchSize: req.body.batchSize,
+      academiaSupported: req.body.academiaSupported,
+      associatedFund: req.body.associatedFund,
+      website: req.body.website,
+      founded: req.body.founded,
+      contact: req.body.contact,
+      bio: req.body.bio
+    }
+  }).spread((service, created) => {
+    if (created) {
+      res.redirect("/services");
+    } else {
+      res.render("registerService", {
+        message: "Service already exists"
+      });
+    }
+  });
 });
 
 // @route POST routes/services/serviceprofile
